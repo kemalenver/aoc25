ba210802e6a5fb0bcbb019c2b57871b7b2550a16
Kemal Enver <kemal.enver@starlingbank.com>
2025-12-02 10:03:40 +1100

    Initial Commit

---
+++ aoc35/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  aoc35
+//
+//  Created by Kemal Enver on 2/12/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
