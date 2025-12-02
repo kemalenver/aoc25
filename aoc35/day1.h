0c30df4a2f0d057ee4b07f7b9bce54c98fe3aee8
Kemal Enver <kemal.enver@starlingbank.com>
2025-12-02 11:16:17 +1100

    Day1-Part1

--- aoc35/day1.h
+++ aoc35/day1.h
@@ -2,8 +2,6 @@
 //  day1.h
 //  aoc35
 //
-//  Created by Kemal Enver on 2/12/2025.
-//
 
 @interface Day1 : NSObject
 
--- aoc35/main.m
+++ aoc35/main.m
@@ -2,15 +2,12 @@
 //  main.m
 //  aoc35
 //
-//  Created by Kemal Enver on 2/12/2025.
-//
 
 #import <Foundation/Foundation.h>
 #import "day1.h"
  
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-
         [Day1 solve];
     }
     return EXIT_SUCCESS;
